c24a647bba107d5b2ed2220ff13d842fe530750d
half6 <christofferhalf6@gmail.com>
2022-10-27 16:10:52 +0200

    fixed nav-bar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('.nav-menu');
 
+
 hamburgerMenu.addEventListener('click', function(){
     hamburgerMenu.classList.toggle('active');
     nav.classList.toggle('active');
+    
     
 })
